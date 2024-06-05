c0fbcf1b967c56a9485fd3b9ed80787a04536f3c
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2024-06-05 23:36:18 +0530

    reduced the no of checks

--- utils/dlr_test.py
+++ utils/dlr_test.py
@@ -40,24 +40,25 @@ fs = lambda y: y / tan(alpha + pi / 2)
 y_incr = floor(n / rise)
 x_incr = floor(n / run)
 
-
-for line in range(max_start_y):
-    a, b = ceil(fs(line)), line
-    # how many times does it stack up in the x axis? (x_incr)
-    # 5 / 4.33 = 1 times
-    # how many times does it stack up in the y axis? (y_incr)
-    # 5 / 2.5 = 2 times
-    # this means, in every 2 iterations, y increases by 1, x increases by 2 itself
-    # more generally, if x_incr/y_incr > 1, (x_incr/y_incr) can be treated as a mod factor wrt x and vice-versa
-    if y_incr > x_incr:
-        mod_factor = y_incr // x_incr
+if y_incr > x_incr:
+    mod_factor = y_incr // x_incr
+    for line in range(max_start_y):
+        a, b = ceil(fs(line)), line
+        # how many times does it stack up in the x axis? (x_incr)
+        # 5 / 4.33 = 1 times
+        # how many times does it stack up in the y axis? (y_incr)
+        # 5 / 2.5 = 2 times
+        # this means, in every 2 iterations, y increases by 1, x increases by 2 itself
+        # more generally, if x_incr/y_incr > 1, (x_incr/y_incr) can be treated as a mod factor wrt x and vice-versa
+        # if y_incr > x_incr:
+        #     mod_factor = y_incr // x_incr
         for pixel in range(n):
             # print(f"{(a + x_offset, b)} maps to {(pixel, line)}, i.e {img[line][pixel]}")
             rot[b][a + x_offset] = img[line][pixel]
             a += 1
             if pixel > 0 and pixel % mod_factor == 0:
                 b += 1
-    # print("----")
+        # print("----")
 
 for y in range(mrt):
     for x in range(nrt):
