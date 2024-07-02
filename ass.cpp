391ad95895ff16dc6992ef9e6b6717bbb82d943a
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2024-07-02 12:27:54 +0530

    ass.cpp works, except z48

--- ass.cpp
+++ ass.cpp
@@ -7,13 +7,6 @@
 using cv::Mat;
 using std::floor, std::ceil, std::abs, std::sin, std::cos, std::tan, std::function;
 
-// int bruh_a(int a, int m) {
-//     return a >= 0 ? a : a + m;
-// }
-
-// int bruh_b(int b, int n) {
-//     return b >= 0 ? b : b + n;
-// }
 
 void rotate(const Mat &img, Mat &rot, int m, int n, int mrt, int nrt, int x_offset, int y_offset, double delta_x, double delta_y, double delta_i,
             function<double(int)> fs, int outer_limit, int last_px,
@@ -25,42 +18,48 @@ void rotate(const Mat &img, Mat &rot, int m, int n, int mrt, int nrt, int x_offs
     int x, y, i_;
 
     if (z48) {
-        // #pragma omp parallel for
-        //         for (int i = outer_limit; i > -1; i--) {
-        //             int index = i - outer_limit;
-        //             double src_i = index * delta_i;
+        // #pragma omp parallel for private(x_, y_, i_, x, y, a, b, a_, b_)
+        for (int i = outer_limit; i > -1; i--) {
+            int index = i - outer_limit;
+            double src_i = index * delta_i;
 
-        //             x_ = fs(i);
-        //             y_ = i;
+            x_ = fs(i);
+            y_ = i;
 
-        //             i_ = floor(src_i);
-        //             for (int px = 0; px < last_px; px++) {
-        //                 x = floor(x_);
-        //                 y = floor(y_);
+            i_ = floor(src_i);
+            for (int px = 0; px < last_px; px++) {
+                x = floor(x_);
+                y = floor(y_);
 
-        //                 a = abs(i_);
-        //                 b = abs(px);
+                a = abs(i_);
+                b = abs(px);
 
-        //                 if (negate_coords) {
-        //                     a_ = mrt - y - y_offset;
-        //                     b_ = nrt - x - x_offset;
-        //                 } else {
-        //                     a_ = y + y_offset;
-        //                     b_ = x + x_offset;
-        //                 }
+                if (negate_coords) {
+                    a_ = mrt - y - y_offset;
+                    b_ = nrt - x - x_offset;
+                } else {
+                    a_ = y + y_offset;
+                    b_ = x + x_offset;
+                }
 
-        //                 if (a_ >= 0 && a_ < mrt && b_ >= 0 && b_ < nrt) {
-        //                     // std::cout << "attempting to map (" << a_ << ", " << b_ << ") to (" << a << ", " << b << ")\n";
-        //                     rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
-        //                 }
-        //                 if (a_ + 1 >= 0 && a_ + 1 < mrt && b_ >= 0 && b_ < nrt)
-        //                     rot.at<cv::Vec3b>(a_ + 1, b_) = img.at<cv::Vec3b>(a, b);
+                if (a < 0)
+                    a += m;
+                if (b < 0)
+                    b += n;
+                
+                // argc: 1
+                // m, n = 2880, 6041 | mrt, nrt = 3886, 6450
 
-        //                 x_ += delta_x;
-        //                 y_ += delta_y;
-        //             }
-        //         }
-                return;
+                if ((a_ >= 0 && a_ < mrt) && (b_ >= 0 && b_ < nrt) &&
+                    (a >= 0 && a < m) && (b >= 0 && b < n)) {
+                    std::cout << "a: " << a << ", b: " << b << ", <- a_: " << a_ << ", b_: " << b_ << "\n";
+                    // rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
+                    // if (a_ + 1 >= 0 && a_ + 1 < mrt && b_ >= 0 && b_ < nrt)
+                    //     rot.at<cv::Vec3b>(a_ + 1, b_) = img.at<cv::Vec3b>(a, b);
+                }
+            }
+        }
+        return;
     }
 
     #pragma omp parallel for private(x_, y_, i_, x, y, a, b, a_, b_)
@@ -71,20 +70,12 @@ void rotate(const Mat &img, Mat &rot, int m, int n, int mrt, int nrt, int x_offs
         y_ = i;
 
         i_ = floor(src_i);
-        // int a, b, a_, b_;
 
         if (flip_range) {
             for (int px = last_px; px > -1; px--) {
                 x = floor(x_);
                 y = floor(y_);
 
-                // int a = rectify_a(horizontal ? i_ : px);
-                // int b = rectify_b(horizontal ? px : i_);
-
-                // int a_ = negate_coords ? mrt - y - y_offset : y + y_offset;
-                // int b_ = negate_coords ? nrt - x - x_offset : x + x_offset;
-
-                // int a, b, a_, b_;
                 if (horizontal) {
                     a = i_;
                     b = px;
@@ -93,8 +84,6 @@ void rotate(const Mat &img, Mat &rot, int m, int n, int mrt, int nrt, int x_offs
                     b = i_;
                 }
 
-                // when do we do this even?
-                // ...when we are fated to deal with negative indices
                 if (negate_coords) {
                     a_ = mrt - y - y_offset;
                     b_ = nrt - x - x_offset;
@@ -108,73 +97,54 @@ void rotate(const Mat &img, Mat &rot, int m, int n, int mrt, int nrt, int x_offs
                 if (b < 0)
                     b += n;
 
-                // a = rectify_a(a);
-                // b = rectify_b(b);
-                // a = bruh_a(a, m);
-                // b = bruh_b(b, n);
-
-                // a_ = y + y_offset;
-                // b_ = x + x_offset;
-                // if (a_ < 0)
-                //     a_ = mrt - y - y_offset;
-                // if (b_ < 0)
-                //     b_ = nrt - x - x_offset;
-
-                // m, n = 2880, 6041
-
-                // #pragma omp critical
-                // {
-                //     int a = rectify_a(horizontal ? i_ : px);
-                //     int b = rectify_b(horizontal ? px : i_);
-                //     int a_ = negate_coords ? mrt - y - y_offset : y + y_offset;
-                //     int b_ = negate_coords ? nrt - x - x_offset : x + x_offset;
-                //     if ((a_ >= 0 && a_ < mrt) && (b_ >= 0 && b_ < nrt) &&
-                //         (a >= 0 && a < m) && (b >= 0 && b < n)) {
-                //         // std::cout << "a: " << a << ", b: " << b << ", <- a_: " << a_ << ", b_: " << b_ << "\n";
-                //         rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
-                //     }
-                // }
-
-                    if ((a_ >= 0 && a_ < mrt) && (b_ >= 0 && b_ < nrt) &&
-                        (a >= 0 && a < m) && (b >= 0 && b < n)) {
-                        // std::cout << "a: " << a << ", b: " << b << ", <- a_: " << a_ << ", b_: " << b_ << "\n";
-                        rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
-                if (a_ + horizontal >= 0 && a_ + horizontal < mrt && b_ + !horizontal >= 0 && b_ + !horizontal < nrt)
-                    rot.at<cv::Vec3b>(a_ + horizontal, b_ + !horizontal) = img.at<cv::Vec3b>(a, b);
-                    }
+                if ((a_ >= 0 && a_ < mrt) && (b_ >= 0 && b_ < nrt) &&
+                    (a >= 0 && a < m) && (b >= 0 && b < n)) {
+                    // std::cout << "a: " << a << ", b: " << b << ", <- a_: " << a_ << ", b_: " << b_ << "\n";
+                    rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
+                    if (a_ + horizontal >= 0 && a_ + horizontal < mrt && b_ + !horizontal >= 0 && b_ + !horizontal < nrt)
+                        rot.at<cv::Vec3b>(a_ + horizontal, b_ + !horizontal) = img.at<cv::Vec3b>(a, b);
+                }
 
                 x_ += delta_x;
                 y_ += delta_y;
             }
         } else {
-            // for (int px = 0; px < last_px; px++) {
-            //     x = floor(x_);
-            //     y = floor(y_);
+            for (int px = 0; px < last_px; px++) {
+                x = floor(x_);
+                y = floor(y_);
 
-            //     if (horizontal) {
-            //         a = i_;
-            //         b = px;
-            //     } else {
-            //         a = px;
-            //         b = i_;
-            //     }
+                if (horizontal) {
+                    a = i_;
+                    b = px;
+                } else {
+                    a = px;
+                    b = i_;
+                }
 
-            //     if (negate_coords) {
-            //         a_ = mrt - y - y_offset;
-            //         b_ = nrt - x - x_offset;
-            //     } else {
-            //         a_ = y + y_offset;
-            //         b_ = x + x_offset;
-            //     }
+                if (negate_coords) {
+                    a_ = mrt - y - y_offset;
+                    b_ = nrt - x - x_offset;
+                } else {
+                    a_ = y + y_offset;
+                    b_ = x + x_offset;
+                }
 
-            //     if (a_ >= 0 && a_ < mrt && b_ >= 0 && b_ < nrt)
-            //         rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
-            //     if (a_ + horizontal >= 0 && a_ + horizontal < mrt && b_ + !horizontal >= 0 && b_ + !horizontal < nrt)
-            //         rot.at<cv::Vec3b>(a_ + horizontal, b_ + !horizontal) = img.at<cv::Vec3b>(a, b);
+                if (a < 0)
+                    a += m;
+                if (b < 0)
+                    b += n;
 
-            //     x_ += delta_x;
-            //     y_ += delta_y;
-            // }
+                if ((a_ >= 0 && a_ < mrt) && (b_ >= 0 && b_ < nrt) &&
+                    (a >= 0 && a < m) && (b >= 0 && b < n)) {
+                    // std::cout << "a: " << a << ", b: " << b << ", <- a_: " << a_ << ", b_: " << b_ << "\n";
+                    rot.at<cv::Vec3b>(a_, b_) = img.at<cv::Vec3b>(a, b);
+                    if (a_ + horizontal >= 0 && a_ + horizontal < mrt && b_ + !horizontal >= 0 && b_ + !horizontal < nrt)
+                        rot.at<cv::Vec3b>(a_ + horizontal, b_ + !horizontal) = img.at<cv::Vec3b>(a, b);
+                }
+
+                x_ += delta_x;
+                y_ += delta_y;
+            }
         }
     }
 }
@@ -192,7 +162,8 @@ int main(int argc, char const *argv[]) {
     int n = img.cols;
 
     // double angle = std::stod(argv[3]);
-    double angle = 226;
+    // double angle = std::stod(argv[1]);
+    double angle = 170;
     double alpha = (M_PI * angle) / 180.0;
 
     double sin_alpha = abs(sin(alpha));
@@ -213,22 +184,22 @@ int main(int argc, char const *argv[]) {
     // function<int(int)> rectify_a = [m](int a) { return a >= 0 ? a : a + m; };
     // function<int(int)> rectify_b = [n](int b) { return b >= 0 ? b : b + n; };
 
-    // // zone 1 and 5
-    // if ((0 <= angle && angle <= 45) || (180 < angle && angle <= 225)) {
-    //     x_offset = ceil(m * sin_alpha);
-    //     y_offset = 0;
-    //     fs = [tan_alpha_](int y) { return y / tan_alpha_; };
-    //     delta_x = cos_alpha;
-    //     delta_y = sin_alpha;
-    //     delta_i = 1 / delta_x;
+    // zone 1 and 5
+    if ((0 <= angle && angle <= 45) || (180 < angle && angle <= 225)) {
+        x_offset = ceil(m * sin_alpha);
+        y_offset = 0;
+        fs = [tan_alpha_](int y) { return y / tan_alpha_; };
+        delta_x = cos_alpha;
+        delta_y = sin_alpha;
+        delta_i = 1 / delta_x;
 
-    //     outer_limit = ceil(m * cos_alpha);
-    //     last_px = n;
-    //     rotate(img, rot, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, true, !(0 <= angle && angle <= 45), false);
-    // }
+        outer_limit = ceil(m * cos_alpha);
+        last_px = n;
+        rotate(img, rot, m, n, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, true, !(0 <= angle && angle <= 45), false);
+    }
 
     // zone 2 and 6
-    if ((45 < angle && angle <= 90) || (225 < angle && angle <= 270)) {
+    else if ((45 < angle && angle <= 90) || (225 < angle && angle <= 270)) {
         x_offset = ceil(m * sin_alpha);
         y_offset = 0;
         fs = [tan_alpha](int y) { return y / tan_alpha; };
@@ -241,34 +212,34 @@ int main(int argc, char const *argv[]) {
         rotate(img, rot, m, n, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, false, false, !(45 < angle && angle <= 90));
     }
 
-    // // zone 3 and 7
-    // else if ((90 < angle && angle <= 135) || (270 < angle && angle <= 315)) {
-    //     x_offset = nrt;
-    //     y_offset = ceil(m * cos_alpha);
-    //     fs = [tan_alpha](int y) { return y / tan_alpha; };
-    //     delta_x = -sin_alpha;
-    //     delta_y = -cos_alpha;
-    //     delta_i = ((90 < angle && angle <= 135) ? -1 : 1) / delta_x;
+    // zone 3 and 7
+    else if ((90 < angle && angle <= 135) || (270 < angle && angle <= 315)) {
+        x_offset = nrt;
+        y_offset = ceil(m * cos_alpha);
+        fs = [tan_alpha](int y) { return y / tan_alpha; };
+        delta_x = -sin_alpha;
+        delta_y = -cos_alpha;
+        delta_i = ((90 < angle && angle <= 135) ? -1 : 1) / delta_x;
 
-    //     outer_limit = ceil(n * sin_alpha);
-    //     last_px = m;
-    //     rotate(img, rot, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, false, false, !(90 < angle && angle <= 135));
-    // }
+        outer_limit = ceil(n * sin_alpha);
+        last_px = m;
+        rotate(img, rot, m, n, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, false, false, !(90 < angle && angle <= 135));
+    }
 
-    // // zone 4 and 8
-    // else if ((135 < angle && angle <= 180) || (315 < angle && angle <= 360)) {
-    //     // std::cout << "z48\n";
-    //     x_offset = ceil(n * cos_alpha);
-    //     y_offset = 0;
-    //     fs = [tan_alpha_](int y) { return y / tan_alpha_; };
-    //     delta_x = -cos_alpha;
-    //     delta_y = sin_alpha;
-    //     delta_i = 1 / delta_x;
+    // zone 4 and 8
+    else if ((135 < angle && angle <= 180) || (315 < angle && angle <= 360)) {
+        // std::cout << "z48\n";
+        x_offset = ceil(n * cos_alpha);
+        y_offset = 0;
+        fs = [tan_alpha_](int y) { return y / tan_alpha_; };
+        delta_x = -cos_alpha;
+        delta_y = sin_alpha;
+        delta_i = 1 / delta_x;
 
-    //     outer_limit = ceil(m * cos_alpha);
-    //     last_px = n;
-    //     rotate(img, rot, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, true, !(135 < angle && angle <= 180), false, true);
-    // }
+        outer_limit = ceil(m * cos_alpha);
+        last_px = n;
+        rotate(img, rot, m, n, mrt, nrt, x_offset, y_offset, delta_x, delta_y, delta_i, fs, outer_limit, last_px, true, !(135 < angle && angle <= 180), false, true);
+    }
 
     // cv::imwrite(argv[2], rot);
     cv::imwrite("../lol.png", rot);
